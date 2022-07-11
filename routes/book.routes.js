d09d9858bf012ac8c3c8147279820d7946826071
jackletran <jack.letran@moebel.de>
2022-07-11 14:53:19 +0200

    add routes dynamically

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -16,4 +16,17 @@ router.get("/books", (req, res, next) => {
     });
 });
 
+router.get("/books/:bookId", (req, res) => {
+  const bookId = req.params.bookId;
+
+  Book.findById(bookId)
+    .then((bookDetails) => {
+      res.render("books/book-details", bookDetails);
+    })
+    .catch((error) => {
+      console.log("Error getting book details from DB", error);
+      next(error);
+    });
+});
+
 module.exports = router;
